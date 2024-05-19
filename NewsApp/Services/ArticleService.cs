8b64518889556bd11463bc2ac3d4d2be0c62f1cc
ART06112006 <artem06112006@gmail.com>
2024-05-19 09:24:38 +0200

    ArticleService

--- NewsApp/Services/ArticleService.cs
+++ NewsApp/Services/ArticleService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NewsAPI;
+using NewsAPI.Constants;
+using NewsAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +11,39 @@ namespace NewsApp.Services
 {
     public class ArticleService
     {
-        
+        private readonly string _apiKey;
+        private Action<string> ExceptionMessage;
+
+
+        public ArticleService(string apiKey, Action<string> exceptionMessage)
+        {
+            _apiKey = apiKey;
+            ExceptionMessage = exceptionMessage;
+        }
+
+        public async Task<(int, List<Article>)> GetArticalsAsync(string searchedText, DateTime fromDate, DateTime toDate, int articalesNumber = 25, SortBys sortBy = SortBys.Relevancy, Languages lang = Languages.EN)
+        {
+            var newsApiClient = new NewsApiClient(_apiKey);
+
+            var articlesResponse = newsApiClient.GetEverything(new EverythingRequest
+            {
+                Q = searchedText,
+                SortBy = sortBy,
+                Language = lang,
+                From = fromDate,
+                To = toDate,
+                PageSize = articalesNumber
+            });
+
+            if (articlesResponse.Status == Statuses.Ok)
+            {
+                return (articlesResponse.TotalResults, articlesResponse.Articles);
+            }
+            else
+            {
+                ExceptionMessage.Invoke(articlesResponse.Error.Message);
+                return (0, null);
+            }
+        }
     }
 }
