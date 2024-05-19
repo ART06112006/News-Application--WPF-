f7ec7cd7bdc2bb955228c09e951d9c2fe7c3b7e9
ART06112006 <artem06112006@gmail.com>
2024-05-19 12:15:08 +0200

    Create ShowArticleViewModel

---
+++ NewsApp/ViewModels/ShowArticleViewModel.cs
@@ -0,0 +1,32 @@
+﻿using NewsAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NewsApp.ViewModels
+{
+    public class ShowArticleViewModel : BaseViewModel
+    {
+        private Article _article;
+        public Article Article
+        {
+            get { return _article; }
+            set
+            {
+                _article = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ExitCommand { get; private set; }
+        public Action CloseView { get; set; }
+
+        public ShowArticleViewModel(ExitShowArticleViewCommand exitShowArticleViewCommand)
+        {
+            ExitCommand = exitShowArticleViewCommand;
+        }
+    }
+}
