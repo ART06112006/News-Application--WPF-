02af71213760606348aebd851891a61dc62bbe4f
ART06112006 <artem06112006@gmail.com>
2024-05-19 11:06:51 +0200

    Create ChangeShowArticleViewModelCommand.cs

---
+++ NewsApp/Commands/ChangeShowArticleViewModelCommand.cs
@@ -0,0 +1,18 @@
+﻿using NewsApp.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApp.Commands
+{
+    public class ChangeShowArticleViewModelCommand : BaseCommand
+    {
+        public override void Execute(object parameter)
+        {
+            var view = (ShowArticleView)(AppServiceProvider.ServiceProvider.GetService(typeof(ShowArticleView)));
+            view.ShowDialog();
+        }
+    }
+}
