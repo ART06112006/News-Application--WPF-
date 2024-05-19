a119b9947710ae02825e2fc7b88289b89703dd0a
ART06112006 <artem06112006@gmail.com>
2024-05-19 10:31:06 +0200

    Create AppServiceProvider.cs

---
+++ NewsApp/Infrastructure/AppServiceProvider.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsApp.Infrastructure
+{
+    public static class AppServiceProvider
+    {
+        public static ServiceProvider ServiceProvider { get; private set; }
+        public static void Initialize()
+        {
+            var services = new ServiceCollection();
+
+            
+
+            ServiceProvider = services.BuildServiceProvider();
+        }
+    }
+}
